450f3305063e57360f3a4f3f5b8d0fd933206c6a
Jordan Robinson <jordanrobinson@Jordans-Air.lan>
2024-02-02 04:47:28 -0600

    created handle click to authenticate token from api in authenticate.jsx

--- src/components/Authenticate.jsx
+++ src/components/Authenticate.jsx
@@ -1,3 +1,17 @@
-export default function Authenticate(){
-    return <h2>Authenticate</h2>
+export default function Authenticate({ token }) {
+  async function handleClick() {
+    console.log("button has been clicked");
+    try {
+    } catch (error) {
+      error.message;
+    }
+  }
+  return (
+    <>
+      <div></div>
+      <h2>Authenticate</h2>
+      {}
+      <button onClick={handleClick}>Authenticate Token</button>
+    </>
+  );
 }
